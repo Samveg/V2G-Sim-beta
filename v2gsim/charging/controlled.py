c3964586836cc7fe6d608c47c0905d7ba144685d
Jonathan56 <jonathan.coignard@gmail.com>
2016-08-09 15:11:32 -0700

    signal charging

--- v2gsim/charging/controlled.py
+++ v2gsim/charging/controlled.py
@@ -207,3 +207,46 @@ def Q_consumption(activity, vehicle, nb_interval, timestep, charging_option):
 
     del SOC[0]  # removed initial value added 17 line above
     return SOC, power_demand
+
+
+def follow_signal(activity, vehicle, nb_interval, timestep, charging_option):
+    """Calculate the consumption of a plugged in vehicle if no control is
+    apply (charge ASAP until full battery).
+
+    Args:
+        activity (Parked): Parked activity to get charging station capabilities
+        vehicle (Vehicle): Vehicle object to get current SOC and physical
+            constraints (maximum SOC, ...)
+        nb_interval (int): number of timestep for the parked activity
+        timestep (int): calculation timestep
+        charging_option (any): not used
+
+    Returns:
+        SOC (list): state of charge
+        power_demand (list): power demand
+    """
+
+    maximum_power = min(activity.charging_station.maximum_power,
+                        vehicle.car_model.maximum_power)
+    power_at_battery = maximum_power * vehicle.car_model.battery_efficiency_charging
+    battery_capacity = vehicle.car_model.battery_capacity * 3600  # from Wh to J
+    signal = charging_option[activity.start:activity:end].signal.tolist()
+
+    SOC = [vehicle.SOC[-1]]
+    power_demand = []
+
+    # For the duration of the activity
+    for i in range(0, nb_interval):
+        # If the car still needs to charge
+        if SOC[-1] < vehicle.car_model.maximum_SOC:
+            # Set the power demand to be the charger station power
+            power_demand.append(maximum_power * signal[i])
+            # SOC [0,1] + (power_demand [W] * timestep [s] / totalCap [J])
+            SOC.append(SOC[-1] + (power_at_battery * signal[i] * timestep / battery_capacity))
+        # Vehicle is not charging
+        else:
+            power_demand.append(0)
+            SOC.append(SOC[-1])
+
+    del SOC[0]  # removed initial value added 17 line above
+    return SOC, power_demand
