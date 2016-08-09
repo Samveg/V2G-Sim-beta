ac7078fc331d8199522ce9923a8a188c388c7f6e
Jonathan56 <jonathan.coignard@gmail.com>
2016-08-09 09:36:18 -0700

    pretty much done with statistic itineraries

--- templates/statistic_itinerary.py
+++ templates/statistic_itinerary.py
@@ -10,31 +10,51 @@ import pdb, traceback, sys
 
 project = v2gsim.model.Project()
 project = v2gsim.itinerary.from_excel(project, '../data/NHTS/Tennessee.xlsx')
+project.vehicles = v2gsim.itinerary.get_cycling_itineraries(project)
 
-# try:
+# Initiate SOC and charging infra
+conv = v2gsim.core.initialize_SOC(project, nb_iteration=1)
+
+# Launch the simulation
+v2gsim.core.run(project)
+
+# Look at the results
+total_power_demand = v2gsim.post_simulation.result.total_power_demand(project)
+
+# Plot the result
+plt.plot(total_power_demand['total'], label='original_power_demand')
+
+# Create a new project
 project.itinerary_statistics = v2gsim.itinerary.find_all_itinerary_combination(project)
-# except:
-#     type, value, tb = sys.exc_info()
-#     traceback.print_exc()
-#     pdb.post_mortem(tb)
+project.itinerary_statistics = v2gsim.itinerary.merge_itinerary_combination(project, ['Home', 'Work'])
+project.itinerary_statistics = v2gsim.itinerary.get_itinerary_statistic(project)
+new_project = v2gsim.itinerary.new_project_using_stats(project, 2000)
 
-# project = v2gsim.itinerary.copy_append(project, nb_copies=2)
 
-# # Initiate SOC and charging infra
-# conv = v2gsim.core.initialize_SOC(project, nb_iteration=1)
+# Initiate SOC and charging infra
+conv = v2gsim.core.initialize_SOC(new_project, nb_iteration=1)
 
-# # Launch the simulation
-# v2gsim.core.run(project, date_from=project.date + datetime.timedelta(hours=12),
-#                 date_to=project.date + datetime.timedelta(days=2, hours=12))
+# Launch the simulation
+import pdb, traceback, sys
+try:
+    v2gsim.core.run(new_project)
+except:
+    type, value, tb = sys.exc_info()
+    traceback.print_exc()
+    pdb.post_mortem(tb)
 
-# # Look at the results
-# total_power_demand = v2gsim.post_simulation.result.total_power_demand(project)
+# Look at the results
+total_power_demand2 = v2gsim.post_simulation.result.total_power_demand(new_project)
 
-# # Plot the result
-# plt.figure()
-# plt.plot(total_power_demand['total'])
-# plt.plot(total_power_demand['Home_demand'])
-# plt.show()
+# Plot the result
+plt.plot(total_power_demand2['total'], label='recreated_power_demand')
+
+plt.show()
 
 print('Press c and then enter to quit debugger')
 pdb.set_trace()
+# project.itinerary_statistics[project.itinerary_statistics.nb_of_vehicles>3]
+# index = 4
+# v2gsim.itinerary.plot_vehicle_itinerary(project.itinerary_statistics.ix[index].vehicles, title=str(project.itinerary_statistics.ix[index].locations))
+# plt.show()
+# v2gsim.itinerary.plot_vehicle_itinerary(new_project.vehicles[:20])
--- v2gsim/itinerary.py
+++ v2gsim/itinerary.py
@@ -1,10 +1,12 @@
 from __future__ import division
 import pandas
+import scipy.stats
 import datetime
 import progressbar
+import matplotlib.pyplot as plt
 import random
 from model import (Vehicle, ChargingStation, Location, Parked, Driving,
-                   BasicCarModel)
+                   BasicCarModel, Project)
 
 
 def from_excel(project, filename):
@@ -71,26 +73,9 @@ def from_excel(project, filename):
             print('Itinerary does not respect the constraints')
             print(vehicle)
         project.vehicles.append(vehicle)
-
+    
     # Initialize charging station at each location
-    project.charging_stations = [ChargingStation(name='no_charger',
-                                                 maximum_power=0,
-                                                 minimum_power=0),
-                                 ChargingStation(name='L1',
-                                                 maximum_power=1440,
-                                                 minimum_power=0),
-                                 ChargingStation(name='L2',
-                                                 maximum_power=7200,
-                                                 minimum_power=0)]
-    df2 = pandas.DataFrame(index=['no_charger', 'L1', 'L2'],
-                           data={'charging_station': project.charging_stations,
-                                 'probability': [0.0, 0.8, 0.2],
-                                 'available': [float('inf'), float('inf'),
-                                               float('inf')],
-                                 'total': [float('inf'), float('inf'),
-                                           float('inf')]})
-    for location in project.locations:
-        location.available_charging_station = df2.copy()
+    reset_charging_infrastructures(project)
 
     print('')
     return project
@@ -116,6 +101,33 @@ def unique_location_category(location_category, project):
     new_location = Location(location_category, name=location_category + '01')
     project.locations.append(new_location)
     return new_location
+
+
+def reset_charging_infrastructures(project):
+    """Reset the charging infrastructure at every location to be uncontrolled charging,
+    using either L1 or L2 charger.
+
+    Args:
+        project (Project): a project
+    """
+    project.charging_stations = [ChargingStation(name='no_charger',
+                                                 maximum_power=0,
+                                                 minimum_power=0),
+                                 ChargingStation(name='L1',
+                                                 maximum_power=1440,
+                                                 minimum_power=0),
+                                 ChargingStation(name='L2',
+                                                 maximum_power=7200,
+                                                 minimum_power=0)]
+    df2 = pandas.DataFrame(index=['no_charger', 'L1', 'L2'],
+                           data={'charging_station': project.charging_stations,
+                                 'probability': [0.0, 0.8, 0.2],
+                                 'available': [float('inf'), float('inf'),
+                                               float('inf')],
+                                 'total': [float('inf'), float('inf'),
+                                           float('inf')]})
+    for location in project.locations:
+        location.available_charging_station = df2.copy()
 
 
 def copy_append(project, nb_copies=2):
@@ -334,7 +346,7 @@ def find_all_itinerary_combination(project, verbose=True):
     frame = pandas.DataFrame(columns={'locations', 'vehicles'})
 
     # Iterate through all the vehicles
-    for vehicle in project.vehicles:
+    for vehicle_index, vehicle in enumerate(project.vehicles):
         itinerary = []
         existing = False
 
@@ -357,7 +369,7 @@ def find_all_itinerary_combination(project, verbose=True):
                     data={'locations': [itinerary], 'vehicles': [[vehicle]]})], axis=0,
                 ignore_index=True)
         if verbose:
-            progress.update(index + 1)
+            progress.update(vehicle_index + 1)
 
     if verbose:
         progress.finish()
@@ -377,7 +389,7 @@ def find_all_itinerary_combination(project, verbose=True):
     return frame
 
 
-def merge_itinerary_combination(project, locations_to_save):
+def merge_itinerary_combination(project, locations_to_save, verbose=True):
     """Merge location combination, do not remove any vehicle.
     itinerary_statistics will only keep the locations in *locations_to_save* or
     *"someWhere"*. Regroup ItineraryBin that are now similar and
@@ -399,6 +411,14 @@ def merge_itinerary_combination(project, locations_to_save):
                 new_locations.append(location_category)
         return new_locations
 
+    def _remove_vehicle_with_missing_driving(values):
+        nb_of_activity = 2 * values['nb_of_parked_activity'] - 1
+        new_vehicles = []
+        for vehicle in values['vehicles']:
+            if len(vehicle.activities) == nb_of_activity:
+                new_vehicles.append(vehicle)
+        return new_vehicles
+
     # Copy the frame
     frame = project.itinerary_statistics.copy()
 
@@ -416,4 +436,288 @@ def merge_itinerary_combination(project, locations_to_save):
                                     'vehicles': [[] for i in range(0, len(unique_locations))]})
 
     # For each of the unique itinerary merge the vehicles from the duplicate itineraries
+    for index, row in frame.iterrows():
+        # Get the corresponding row in frame 2 and append vehicles
+        temp_index = frame2[frame2.locations == row.locations].index
+        if len(temp_index) > 1:
+            print('Error one the itinerary is not unique')
+        frame2.loc[temp_index[0], 'vehicles'].extend(row.vehicles)
+
+    # Get some basic filtering value
+    frame2['nb_of_parked_activity'] = (
+        frame2.locations.apply(lambda x: len(x)))
+
+    # Check that all vehicles have the same number of activities (perhaps one is missing a driving)
+    frame2.vehicles = frame2.apply(_remove_vehicle_with_missing_driving, axis=1)
+
+    frame2['nb_of_vehicles'] = (
+        frame2.vehicles.apply(lambda x: len(x)))
+
+    if verbose:
+        print(str(len(project.vehicles) - frame2.nb_of_vehicles.sum()) +
+              'removed vehicles, because of missing driving cycle')
+
+    frame2['worker'] = (
+        frame2.locations.apply(lambda x: True if 'Work' in x else False))
+
+    return frame2
+
+
+def get_itinerary_statistic(project, verbose=True):
+    """For each itinerary creates a specific dataframe containing
+    primary statistical metrics on each activity using data from all the 
+    vehicles in the itinerary's category. Note date are assumed to be UTC.
+
+    Args:
+        project (Project): a project
     
+    Returns:
+        a pandas DataFrame with locations, vehicles, high level statistics and detailed
+        activity statistics.
+    """
+    
+    # Copy the data
+    frame = project.itinerary_statistics.copy()
+
+    # Get the total number of vehicles
+    total_vehicle = frame.nb_of_vehicles.sum()
+    if total_vehicle != len(project.vehicles):
+        print('Warning vehicles are missing: ' +
+            str(total_vehicle - len(project.vehicles)) + ' vehicles')
+
+    # For each combination
+    epoch = datetime.datetime.utcfromtimestamp(0)  # Needed to change dates into floats (assume dates are UTC)
+    temp_frame_holding_stat = pandas.DataFrame()  # Needed because coulc not update dataframe with another frame
+    for index, row in frame.iterrows():
+        # Create the structure holding the results for the whole day
+        total_number_of_activities = (row.nb_of_parked_activity * 2) - 1
+        activity_stat = pandas.DataFrame(index=range(0, total_number_of_activities),
+                                         columns=['start_loc', 'start_scale', 
+                                                  'duration_loc', 'duration_scale',
+                                                  'end_loc', 'end_scale',
+                                                  'distance_loc', 'distance_scale',
+                                                  'mean_speed_loc', 'mean_speed_scale'])
+
+        # Iterate through all the activities for each vehicle to gather the data (could skip this)
+        vehicle_schedules = []
+        for vehicle in row.vehicles:
+            vehicle_stat = pandas.DataFrame(index=range(0, len(vehicle.activities)),
+                                            columns=['start', 'duration', 'end', 'distance', 'mean_speed'])
+            vehicle_stat = vehicle_stat.fillna(0)
+            for i, activity in enumerate(vehicle.activities):
+                if isinstance(activity, Parked):
+                    vehicle_stat.loc[i, 'start'] = (activity.start - epoch).total_seconds()
+                    vehicle_stat.loc[i, 'duration'] = (activity.end - activity.start).total_seconds()
+                    vehicle_stat.loc[i, 'end'] = (activity.end - epoch).total_seconds()
+
+                elif isinstance(activity, Driving):
+                    vehicle_stat.loc[i, 'start'] = (activity.start - epoch).total_seconds()
+                    vehicle_stat.loc[i, 'duration'] = (activity.end - activity.start).total_seconds()    
+                    vehicle_stat.loc[i, 'distance'] = activity.distance
+                    if vehicle_stat.loc[i, 'duration'] == 0:
+                        vehicle_stat.loc[i, 'mean_speed'] = 0
+                    else:
+                        vehicle_stat.loc[i, 'mean_speed'] = activity.distance / ((activity.end - activity.start).total_seconds() / 3600)
+                    vehicle_stat.loc[i, 'end'] = (activity.end - epoch).total_seconds()
+            vehicle_schedules.append(vehicle_stat)
+
+        # Get the best fit for each of the category per activity
+        for activity_index in range(0, total_number_of_activities):
+            for column in ['start', 'duration', 'end', 'distance', 'mean_speed']:
+                temporary_vector = []
+                for schedule in vehicle_schedules:
+                    temporary_vector.append(schedule.loc[activity_index, column])
+                # Find the best fit for the data
+                loc, scale = scipy.stats.norm.fit(temporary_vector)
+                activity_stat.loc[activity_index, column + str('_loc')] = loc
+                activity_stat.loc[activity_index, column + str('_scale')] = scale
+
+        temp_frame_holding_stat = pandas.concat([temp_frame_holding_stat, 
+                                                 pandas.DataFrame(index=[index],
+                                                                  data={'activity_statistics': [activity_stat]})], axis=0)
+
+    frame = pandas.concat([frame, temp_frame_holding_stat], axis=1)
+
+    return frame
+
+
+def create_vehicles_from_stat(current_id, itinerary, project):
+    """Create one vehicle from a statistical itinerary
+
+    Return:
+        a vehicle object with a full day of activities
+    """
+    not_completed = True
+    max_iteration = 100
+    iteration = 0
+
+    while not_completed or iteration < max_iteration:
+        iteration += 1
+        vehicle = Vehicle(current_id, project.car_models[0])
+
+        is_first_activity = lambda x: True if x == 0 else False
+        is_last_activity = lambda x, y=len(itinerary.activity_statistics) - 1: True if x == y else False
+        is_duration_enough = lambda x: x if x > 60 else 60
+                    
+        # The activities one by one
+        location_index = 0
+        parked = True
+        try_again = False
+        for index, activity in itinerary.activity_statistics.iterrows():
+            if is_first_activity(index):
+                # This activity is assumed to be parked
+                start = project.date
+                # duration is int() to avoid milliseconds 
+                duration = int(scipy.stats.norm.rvs(activity.duration_loc, activity.duration_scale, 1)[0])
+                # duration is rouded at project.timestep
+                duration -= duration % project.timestep
+                duration = is_duration_enough(duration)
+                end = start + datetime.timedelta(seconds=duration)
+                location = itinerary.locations[location_index]
+                location_index += 1
+                parked = False
+                
+                # Append the activity
+                vehicle.activities.append(Parked(start, end, unique_location_category(location, project)))
+
+            elif is_last_activity(index):
+                # This activity is assumed to be parked
+                start = vehicle.activities[-1].end
+                end = project.date + datetime.timedelta(days=1)
+                location = itinerary.locations[location_index]
+                
+                # Append the activity
+                vehicle.activities.append(Parked(start, end, unique_location_category(location, project)))
+
+            elif parked:
+                start = vehicle.activities[-1].end
+                duration = int(scipy.stats.norm.rvs(activity.duration_loc, activity.duration_scale, 1)[0])
+                duration -= duration % project.timestep
+                duration = is_duration_enough(duration)
+                end = start + datetime.timedelta(seconds=duration)
+                location = itinerary.locations[location_index]
+                location_index += 1
+                parked = False
+                
+                # Append the activity
+                vehicle.activities.append(Parked(start, end, unique_location_category(location, project)))
+
+            elif not parked:
+                start = vehicle.activities[-1].end
+                duration = int(scipy.stats.norm.rvs(activity.duration_loc, activity.duration_scale, 1)[0])
+                duration -= duration % project.timestep
+                duration = is_duration_enough(duration)
+                mean_speed = scipy.stats.norm.rvs(activity.mean_speed_loc, activity.mean_speed_scale, 1)[0]
+                end = start + datetime.timedelta(seconds=duration)
+                distance = (duration / 3600) * mean_speed  # duration in seconds and mean_speed in km/h
+                vehicle.activities.append(Driving(start, end, distance))
+                parked = True
+
+            # Post activity check
+            if end > project.date + datetime.timedelta(days=1):
+                # Some activity took too much time
+                try_again = True
+                break
+        
+        if not try_again:
+            not_completed = False
+
+    return vehicle
+
+
+def new_project_using_stats(old_project, new_number_of_vehicle, only_worker=False, 
+                            min_vehicles_per_bin=False, remove_old_project=False, 
+                            advanced_filtering_func=False, creating_vehicle_func=create_vehicles_from_stat):
+    """Creates a new project from statistical itineraries of an existing project.
+    Note: In the default version, itinerary should be able to cycle day after day, see
+    get_cycling_itineraries.
+
+    Args:
+        old_project (Project): project which contains the statistical itineraries
+        new_number_of_vehicle (Integer): number of vehicles to create from the statistics
+        only_worker (Boolean): remove non worker from the pool
+        min_vehicles_per_bin (Integer): remove itineraries with too few vehicle representing it
+        remove_old_project (Boolean): remove the old project to free the memory before creating 
+            the new project.
+        advanced_filtering_func (Function): customizable filtering function
+
+    Return:
+        a new project with similar itineraries as the old project and chosen number of vehicles
+    """
+    # Create a new project
+    project = Project()
+    project.car_models = [BasicCarModel(name='Leaf')]
+
+    # Copy the data
+    frame = old_project.itinerary_statistics.copy()
+
+    # Remove the old project ?
+    if remove_old_project:
+        del old_project  # Deep del?
+
+    # Filtering the statistical data
+    if only_worker:
+        frame = frame[frame.worker == True]
+
+    if min_vehicles_per_bin:
+        frame = frame[frame.nb_of_vehicles >= min_vehicles_per_bin]
+
+    if advanced_filtering_func:
+        frame = advanced_filtering_func(frame)
+    
+    # For each itinerary of the frame recreate
+    current_id = 0
+    total_number_of_vehicle = frame.nb_of_vehicles.sum()
+    for index, itinerary in frame.iterrows():
+        # How many vehicles should be created
+        number_of_vehicle_per_bin = int((itinerary.nb_of_vehicles / total_number_of_vehicle) * new_number_of_vehicle)
+
+        # Recreate the vehicles activities
+        for vehicle_index in range(0, number_of_vehicle_per_bin):
+            vehicle = creating_vehicle_func(current_id, itinerary, project)
+
+            # Check time gap before appending vehicle to the project
+            if not vehicle.check_activities(
+                start_date=project.date, end_date=project.date + datetime.timedelta(days=1)):
+                print('Itinerary does not respect the constraints')
+                print(vehicle)
+                import pdb
+                pdb.set_trace()
+            project.vehicles.append(vehicle)
+            current_id += 1
+
+    # Initialize charging station at each location
+    reset_charging_infrastructures(project)
+
+    return project
+
+
+def plot_vehicle_itinerary(vehicles, title='No title'):
+    """Plot the itinerary of a vehicle through out the day
+
+    Args:
+        vehicles (List): list of vehicles
+    """
+    for index, vehicle in enumerate(vehicles):
+        # Create the vector to plot
+        x = []
+        y = []
+        for activity in vehicle.activities:
+            x.append(activity.start)
+            x.append(activity.end)
+            if isinstance(activity, Parked):
+                y.append(0 + index)
+                y.append(0 + index)
+            if isinstance(activity, Driving):
+                y.append(1 + index)
+                y.append(1 + index)
+        plt.plot(x, y, label='vehicle id ' + str(vehicle.id), linewidth=2.0, alpha=0.8)
+    plt.legend(loc=0)
+    plt.title(title)
+
+
+
+
+
+
+
