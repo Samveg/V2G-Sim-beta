59bf7375b071d4c75b12a68ef5a7e07354882496
Jonathan56 <jonathan.coignard@gmail.com>
2016-08-25 10:49:48 -0700

    cleaning decentralized optimization

--- templates/8_decentralized_optimization.py
+++ templates/8_decentralized_optimization.py
@@ -7,7 +7,7 @@ import pandas
 
 # Create a project and initialize it with someitineraries
 project = v2gsim.model.Project()
-project = v2gsim.itinerary.from_excel(project, '../data/NHTS/Tennessee_100.xlsx')
+project = v2gsim.itinerary.from_excel(project, '../data/NHTS/Tennessee.xlsx')
 
 # This function from the itinerary module return all the vehicles that
 # start and end their day at the same location (e.g. home)
@@ -28,7 +28,7 @@ project.charging_stations.extend(charging_stations)
 # apply this mix at all the locations
 df = pandas.DataFrame(index=['L2', 'L1_V1G', 'L2_V2G'],
                       data={'charging_station': charging_stations,
-                            'probability': [0.3, 0.2, 0.5]})
+                            'probability': [0.0, 0.4, 0.6]})
 for location in project.locations:
     if location.category in ['Work', 'Home']:
         location.available_charging_station = df.copy()
@@ -46,21 +46,19 @@ for vehicle in project.vehicles:
     for activity in vehicle.activities:
         if isinstance(activity, v2gsim.model.Parked):
             if activity.charging_station.post_simulation:
-                vehicle.result_function = v2gsim.post_simulation.temp_netload_optimization.save_vehicle_state_for_decentralized_optimization
+                vehicle.result_function = v2gsim.post_simulation.netload_optimization.save_vehicle_state_for_decentralized_optimization
                 # pass to the next vehicle
                 break
 
 # Launch the simulation
-# Note that date_from and date_to have been added since 1_basic_template.py
-# date_from and date_to allows a user to specify
-# the window over which results are saved (reduce memory burden).
 v2gsim.core.run(project)
 
 # Look at the results
 total_power_demand = v2gsim.post_simulation.result.total_power_demand(project)
 
 # Create the optimization object
-opti = v2gsim.post_simulation.temp_netload_optimization.DecentralizedOptimization(project, 10)
+opti = v2gsim.post_simulation.netload_optimization.DecentralizedOptimization(
+    project, 10, max_iteration=15, sigma=50)
 
 # Load the net load data
 finalResult = pandas.DataFrame()
@@ -71,7 +69,8 @@ day = datetime.datetime(2025, 6, 17)
 net_load = pandas.DataFrame(net_load[day: day + datetime.timedelta(days=1)]['netload'])
 
 # Initialize the optimization
-vehicle_load, total_vehicle_load, net_load_with_vehicles = opti.solve(project, net_load / (1000 * 1000), 1500000)
+new_net_load, opti_vehicle_load, total_vehicle_load, net_load_with_vehicles, dual_gap = opti.solve(
+    project, net_load * (1000 * 1000), real_number_of_vehicle=1500000)
 
 import pdb
 pdb.set_trace()
